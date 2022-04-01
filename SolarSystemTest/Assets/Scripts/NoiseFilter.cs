f505a096df7284d32842baee29a2b12465c9d392
Reagan Scoles <reagan.scoles@gmail.com>
2022-03-31 20:13:32 -0400

    More noise settings

--- SolarSystemTest/Assets/Scripts/NoiseFilter.cs
+++ SolarSystemTest/Assets/Scripts/NoiseFilter.cs
@@ -14,7 +14,21 @@ public class NoiseFilter
 
     public float Evaluate(Vector3 point)
     {
-        float noiseValue = (noise.Evaluate(point * settings.roughness + settings.center) + 1) * 0.5f;
+        //float noiseValue = (noise.Evaluate(point * settings.roughness + settings.center) + 1) * 0.5f;
+        float noiseValue = 0;
+        float frequency = settings.baseRoughness;
+        float amplitude = 1.0f;
+
+        for(int i = 0; i < settings.numLayers; i++)
+        {
+            float v = noise.Evaluate(point * frequency + settings.center);
+            noiseValue += (v + 1) * 0.5f * amplitude;
+            frequency *= settings.roughness;
+            amplitude *= settings.persistence;
+        }
+
+        //noiseValue = Mathf.Max(0, noiseValue - settings.minValue);
+
         return noiseValue * settings.strength;
     }
 }
--- SolarSystemTest/Assets/Scripts/NoiseSettings.cs
+++ SolarSystemTest/Assets/Scripts/NoiseSettings.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 public class NoiseSettings
 {
     public float strength = 1.0f;
-    public float roughness = 1.0f;
+    [Range(1, 8)]
+    public int numLayers = 1;
+    public float baseRoughness = 1.0f;
+    public float roughness = 2.0f;
+    public float persistence = 0.5f;
     public Vector3 center;
-
+    public float minValue;
 
 }
